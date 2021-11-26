66c92f3bb1ecae4bfc6d2b93e0a68dd341d0277a
vitor <vhrocha17@gmail.com>
2021-11-26 14:33:47 -0300

    fazendo o commit do next

---
+++ pages/basicos/jsx1.jsx
@@ -0,0 +1,4 @@
+//jsx
+export default function jsx1Diferente() {
+  return <h1>Jsx #1</h1>
+}
---
+++ pages/basicos/jsx3.jsx
@@ -0,0 +1,7 @@
+export default function jsx3() {
+  return (
+    <div>
+      <h1>Jsx #3</h1>
+    </div>
+  )
+}
